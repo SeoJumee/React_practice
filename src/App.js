e2448230bb165d7d875e26a43a4b5c1d1be3893b
서주미 <official.jumee@gmail.com>
2023-02-11 18:50:50 +0900

    useRef를 이용한 DOM 접근하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import UserList from './UserList';
 
 function App() {
-  return <UserList />;
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+    },
+  ];
+
+  const nextId = useRef(4); //useRef()의 파라미터 = .current의 기본값 (조회, 수정시에 .current 사용)
+  const onCreate = () => {
+    nextId.current += 1;
+  };
+  return <UserList users={users} />;
 }
 
 export default App;

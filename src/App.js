cb5d1e45ec67932ec2ccdde3dfb3ed0486bd516a
서주미 <official.jumee@gmail.com>
2023-02-12 17:41:08 +0900

    배열에 항목 추가하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+  const { username, email } = inputs;
+  const onChange = (e) => {
+    const { name, value } = e.target; //e.target은 특정 이벤트가 가진 태그를 가르킴
+    setInputs({
+      ...inputs, // spread를 이용해 inputs를 복사
+      [name]: value,
+    });
+  };
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: 'velopert',
@@ -18,13 +31,35 @@ function App() {
       username: 'liz',
       email: 'liz@example.com',
     },
-  ];
+  ]);
 
   const nextId = useRef(4); //useRef()의 파라미터 = .current의 기본값 (조회, 수정시에 .current 사용)
   const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    // setUsers([...users, user]); //spread
+    setUsers(users.concat(user)); //concat
+
+    setInputs({
+      username: '',
+      email: '',
+    });
     nextId.current += 1;
   };
-  return <UserList users={users} />;
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
+    </>
+  );
 }
 
 export default App;
